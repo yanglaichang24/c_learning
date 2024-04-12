d588f07d58c6e3683abebb92170ef54b4e0d0a94
LAICHANG YANG <yanglaichang24@gmail.com>
2024-04-12 16:01:43 +0800

    syslib

---
+++ syslib.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// 调用win系统命令
+int main(int argc, char const *argv[])
+{
+	/* code */
+
+    //win calc
+    system("calc");
+
+    system(" cmd ");
+
+     // linux 
+    //system("lc");
+
+	return 0;
+}
